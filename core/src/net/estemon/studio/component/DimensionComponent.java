24f84d64f9976fe1768700b599b68a2b4c544c75
aaronFortuno <aaron.fortuno@protonmail.com>
2023-12-22 20:11:04 +0100

    Adding components for snake entity

---
+++ core/src/net/estemon/studio/component/DimensionComponent.java
@@ -0,0 +1,19 @@
+package net.estemon.studio.component;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.utils.Pool;
+
+public class DimensionComponent implements Component, Pool.Poolable {
+
+    // attributes
+    public float width = 1f;
+    public float height = 1f;
+
+    // public methods
+
+    @Override
+    public void reset() {
+        width = 1f;
+        height = 1f;
+    }
+}
---
+++ core/src/net/estemon/studio/component/PositionComponent.java
@@ -0,0 +1,18 @@
+package net.estemon.studio.component;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.utils.Pool;
+
+public class PositionComponent implements Component, Pool.Poolable {
+
+    // attributes
+    public float x;
+    public float y;
+
+    // public methods
+    @Override
+    public void reset() {
+        x = 0f;
+        y = 0f;
+    }
+}
