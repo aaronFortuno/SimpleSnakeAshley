dc08da0cb39bd0c56d20cc764605e11be8e6af02
aaronFortuno <aaron.fortuno@protonmail.com>
2023-12-23 12:36:28 +0100

    Improving ZOrder rendering system by extending SortedIteratingSystem

--- core/src/net/estemon/studio/system/RenderSystem.java
+++ core/src/net/estemon/studio/system/RenderSystem.java
@@ -3,6 +3,7 @@ package net.estemon.studio.system;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.ashley.systems.SortedIteratingSystem;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -14,7 +15,7 @@ import net.estemon.studio.component.ZOrderComponent;
 import net.estemon.studio.utils.Mappers;
 import net.estemon.studio.utils.ZOrderComparator;
 
-public class RenderSystem extends IteratingSystem {
+public class RenderSystem extends SortedIteratingSystem {
 
     // constants
     private static final Family FAMILY = Family.all(
@@ -27,44 +28,33 @@ public class RenderSystem extends IteratingSystem {
     // attributes
     private final SpriteBatch batch;
     private final Viewport viewport;
-    private final Array<Entity> renderQueue = new Array<Entity>();
 
     public RenderSystem(SpriteBatch batch, Viewport viewport) {
-        super(FAMILY);
+        super(FAMILY, ZOrderComparator.INSTANCE);
         this.batch = batch;
         this.viewport = viewport;
     }
 
     @Override
     public void update(float deltaTime) {
-        super.update(deltaTime);
-        renderQueue.sort(ZOrderComparator.INSTANCE);
-        draw();
-        renderQueue.clear();
-    }
-
-    @Override
-    protected void processEntity(Entity entity, float deltaTime) {
-        renderQueue.add(entity);
-    }
-
-    // private methods
-    private void draw() {
         viewport.apply();
         batch.setProjectionMatrix(viewport.getCamera().combined);
         batch.begin();
 
-        for (Entity entity : renderQueue) {
-            PositionComponent position = Mappers.POSITION.get(entity);
-            DimensionComponent dimension = Mappers.DIMENSION.get(entity);
-            TextureComponent texture = Mappers.TEXTURE.get(entity);
-
-            batch.draw(texture.region,
-                    position.x, position.y,
-                    dimension.width, dimension.height
-            );
-        }
+        super.update(deltaTime);
 
         batch.end();
+    }
+
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        PositionComponent position = Mappers.POSITION.get(entity);
+        DimensionComponent dimension = Mappers.DIMENSION.get(entity);
+        TextureComponent texture = Mappers.TEXTURE.get(entity);
+
+        batch.draw(texture.region,
+                position.x, position.y,
+                dimension.width, dimension.height
+        );
     }
 }
